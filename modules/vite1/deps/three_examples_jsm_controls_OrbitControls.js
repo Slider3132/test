cd3d4b32c82d82e549e36e20201b3c5d2d3b9bca
IPotov <iliya3132@gmail.com>
2024-06-07 16:11:31 +0300

    1

---
+++ modules/vite1/deps/three_examples_jsm_controls_OrbitControls.js
@@ -0,0 +1,4 @@
+import './modules/vite1/deps/chunk-BYJ7EWHE.js?v=a5859036';
+import { OrbitControls } from './modules/vite1/deps/chunk-H6LP4O7T.js?v=a5859036';
+export { OrbitControls };
+//# sourceMappingURL=three_examples_jsm_controls_OrbitControls.js.map
